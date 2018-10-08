d11e8d2b5403c3eb8301b59c5aa6a506c7ce07c6
Ekaterina Shevarova <kat121193@yandex.ru>
2018-10-08 19:27:27 +0300

    delete unnecessary files

--- get_round.py
+++
@@ -1,8 +0,0 @@
-def get_round50(str_num):
-    try:
-        num = int(str_num)
-    except ValueError:
-        num = int(float(str_num))
-    while num % 50 != 0:
-        num += 1
-    return num
