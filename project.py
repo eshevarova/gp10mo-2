156f9d103e5156528848a47ab98d459f70256fdd
Ekaterina Shevarova <kat121193@yandex.ru>
2018-10-08 19:23:42 +0300

    Change sms-function

---
+++ project.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, Response
+
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return 'Hello!'
+
+
+@app.route("/clients", methods =['POST', 'GET'])
+def get_client_data():
+    if request.method == 'POST':
+        data = request.get_json()
+        if data['name']:
+            name = data['name']
+        else:
+            return json.dumps({'status': 'fail', 'message': 'Empty value of name'})
+        if data['tel']:
+            tel = data['tel']
+        else:
+            return json.dumps({'status': 'fail', 'message': 'Empty value of tel'})
+        with open('clients.txt', 'a', encoding='utf-8') as f:
+            f.write("%s %s\n" % (name, tel))
+        return json.dumps({'status': 'ok', 'message': 'We got your request'})
+    else:
+        return 'Do not use GET, use POST'
+
+@app.route("/sms", methods = 'POST')
+def get_sms_data():
+
+        
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
