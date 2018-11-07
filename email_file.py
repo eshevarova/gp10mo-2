8caff152a7bdf569468df9f3099bb3ea5592e760
Ekaterina <kat121193@yandex.ru>
2018-11-07 14:25:41 +0300

    Fix unicode error

--- email_file.py
+++ email_file.py
@@ -4,40 +4,31 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 import logging
-from project_files.mail_data import login as log
-from project_files.mail_data import password as psw
-from project_files.mail_data import smtpserver as smtp
-from project_files.mail_data import from_address as fa
-from project_files.mail_data import to_address as ta
+from project_files.mail_parameters import *
+
 
 logging.basicConfig(filename='attachment.log', level=logging.ERROR, format=' %(asctime)s - %(levelname)s - %(messages)s')
 
 
-def send_attachment():
-    from_address = fa  # от кого
-    to_address = ta  # кому
-    subject = 'Test email with attachment'  # тема письма
-    login = log  # логин на сервере
-    password = psw  # пароль
-    smtpserver = smtp  # адрес smtp сервера
+def send_attachment(email, subject, text, file_path):
+    from_address = FROM_WHOM_EMAIL  # от кого
+    login = LOGIN_EMAIL  # логин на сервере
+    password = PASSWORD_EMAIL  # пароль
+    smtpserver = SMTPSERVER  # адрес smtp сервера
 
     message = MIMEMultipart()
 
     message['From'] = from_address
-    message['To'] = to_address
+    message['To'] = email
     message['Subject'] = subject
 
-    body = 'Проверка проверка проверка '  # email text
-
-    message.attach(MIMEText(body, 'plain'))
+    message.attach(MIMEText(text, 'plain', 'utf-8'))
     try:
-        file_name = 'project_files/db_model.pdf'  # file path with extension
-
-        with open(file_name, 'rb') as attachment:
+        with open(file_path, 'rb') as attachment:
             p = MIMEBase('application', 'octet-stream')
             p.set_payload(attachment.read())
             encoders.encode_base64(p)
-            p.add_header('Content-Disposition', "attachment; filename= {}".format(file_name))
+            p.add_header('Content-Disposition', "attachment; filename= {}".format(file_path))
             message.attach(p)
     except FileNotFoundError as err:
         print(err)
@@ -47,8 +38,7 @@ def send_attachment():
     try:
         server.starttls()
         server.login(login, password)
-        text = message.as_string()
-        server.sendmail(from_address, to_address, text)
+        server.sendmail(from_address, email, message.as_string())
     except smtplib.SMTPHeloError as err1:
         logging.error(err1)
     except smtplib.SMTPAuthenticationError as err2:
@@ -57,7 +47,3 @@ def send_attachment():
         logging.error(err3)
     finally:
         server.quit()
-
-
-if __name__ == '__main__':
-    send_attachment()
--- email_text.py
+++ email_text.py
@@ -4,33 +4,24 @@ send email with Python
 
 import smtplib
 from email.mime.text import MIMEText
+from email.header import Header
+from project_files.mail_parameters import *
 import logging
-import netrc
 
 logging.basicConfig(filename='email.log', level=logging.ERROR, format=' %(asctime)s - %(levelname)s - %(messages)s')
 
 
-def sendmail(email):
-    """
-    Отправка сообщения
-    :return:
-    """
-    from_address = ''  # от кого
-    to_address = email  # кому
-    subject = ''  # тема письма
-    login = ''  # логин на сервере
-    password = ''  # пароль
-    smtpserver = ''  # адрес smtp сервера
+def sendmail(email, subject, message):
+    from_address = FROM_WHOM_EMAIL  # от кого
+    login = LOGIN_EMAIL  # логин на сервере
+    password = PASSWORD_EMAIL  # пароль
+    smtpserver = SMTPSERVER  # адрес smtp сервера
 
-    message = " "  # текст сообщения
     message = MIMEText(message, 'plain', 'utf-8')  # кодировка
+    message['Subject'] = Header(subject, 'utf-8')
+    message['From'] = from_address
+    message['To'] = email
 
-    mail_body = '\r\n'.join((
-        'From: {}'.format(from_address),
-        'To: {}'.format(to_address),
-        'Subject: {}'.format(subject),
-        message.as_string()  # конвертация в строку
-    ))
 
     server = smtplib.SMTP(smtpserver)
     server.set_debuglevel(1)
@@ -38,7 +29,7 @@ def sendmail(email):
     try:
         server.starttls()
         server.login(login, password)
-        server.sendmail(from_address, to_address, mail_body)
+        server.sendmail(from_address, email, message.as_string())
     except smtplib.SMTPHeloError as err1:
         logging.error(err1)
     except smtplib.SMTPAuthenticationError as err2:
@@ -47,4 +38,3 @@ def sendmail(email):
         logging.error(err3)
     finally:
         server.quit()
-
